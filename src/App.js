ee27aa97d4706f291694f2da9d38f66bbb8476e8
Kanstantsin Shchuchkin <kanstantsin.shchuchkin@pricehubble.com>
2022-04-03 11:56:48 +0500

    Add navigation withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
